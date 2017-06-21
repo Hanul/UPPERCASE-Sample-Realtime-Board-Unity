9eae895c9a552fd1e5dbcbe5386ddfcaadb3b7e3
Hanul <hanul@hanul.me>
2017-06-21 19:01:08 +0900

    Room 추가

--- Assets/Uniconn/Model/Model.cs
+++ Assets/Uniconn/Model/Model.cs
@@ -3,11 +3,72 @@
     public class Model
     {
         public delegate void ErrorHandler(string errorMsg);
-
         public delegate void NotExistsHandler();
-
         public delegate void NotAuthedHandler();
-
         public delegate void Handler(object data);
+
+        private RoomConnector connector;
+        private string boxName;
+        private string name;
+
+        private Room room;
+
+        public Model(RoomConnector connector, string boxName, string name)
+        {
+            this.connector = connector;
+            this.boxName = boxName;
+            this.name = name;
+
+            room = new Room(connector, boxName, name);
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public Room GetRoom()
+        {
+            return room;
+        }
+
+        public void Create(object data, ErrorHandler errorHandler, NotAuthedHandler notAuthedHandler, Handler handler)
+        {
+            room.Send("create", data, (object result) =>
+            {
+                /*try
+                {
+                    CreateHandler h = handler;
+                    if (h == null)
+                    {
+                        h = new CreateHandler()
+                        {
+                        };
+                    }
+
+                    if (result.isNull("errorMsg") != true)
+                    {
+                        h.error(result.getString("errorMsg"));
+                    }
+                    else if (result.isNull("validErrors") != true)
+                    {
+                        h.notValid(result.getJSONObject("validErrors"));
+                    }
+                    else if (result.isNull("isNotAuthed") != true && result.getBoolean("isNotAuthed") == true)
+                    {
+                        h.notAuthed();
+                    }
+                    else
+                    {
+                        h.success(result.getJSONObject("savedData"));
+                    }
+
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }*/
+            });
+        }
     }
 }
--- Assets/Uniconn/Room/Room.cs
+++ Assets/Uniconn/Room/Room.cs
@@ -1,7 +1,103 @@
-﻿namespace Uniconn
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Uniconn
 {
     public class Room
     {
-        
+        private RoomConnector connector;
+
+        private string roomName;
+        private Dictionary<string, List<object>> methodHandlerMap = new Dictionary<string, List<object>>();
+        private bool exited;
+
+        public Room(RoomConnector connector, string boxName, string name)
+        {
+            this.connector = connector;
+            connector.EnterRoom(roomName = boxName + "/" + name);
+        }
+
+        public void On<T>(string methodName, Connector.MethodHandler<T> methodHandler)
+        {
+            List<object> methodHandlers = methodHandlerMap[roomName + "/" + methodName];
+
+            connector.On(roomName + "/" + methodName, methodHandler);
+
+            if (methodHandlerMap[roomName + "/" + methodName] == null)
+            {
+                methodHandlerMap.Add(roomName + "/" + methodName, methodHandlers = new List<object>());
+            }
+
+            methodHandlers.Add(methodHandler);
+        }
+
+        public void Off(string methodName, object methodHandler)
+        {
+            List<object> methodHandlers = methodHandlerMap[roomName + "/" + methodName];
+
+            connector.Off(roomName + "/" + methodName, methodHandler);
+
+            methodHandlers.Remove(methodHandler);
+
+            if (methodHandlers.Count == 0)
+            {
+                Off(methodName);
+            }
+        }
+
+        public void Off(string methodName)
+        {
+            List<object> methodHandlers = methodHandlerMap[roomName + "/" + methodName];
+
+            foreach (object methodHandler in methodHandlers)
+            {
+                connector.Off(roomName + "/" + methodName, methodHandler);
+            }
+
+            methodHandlerMap.Remove(roomName + "/" + methodName);
+        }
+
+        public void Send<T>(string methodName, object data, Connector.MethodHandler<T> methodHandler)
+        {
+            if (exited != true)
+            {
+                connector.Send(roomName + "/" + methodName, data, methodHandler);
+            }
+            else
+            {
+                // TODO: 테스트 해봐야함
+                Debug.LogError("ROOM.send에 실패하였습니다. 이미 룸에서 나갔습니다,");
+            }
+        }
+
+        public void Send(string methodName, object data)
+        {
+            Send<object>(methodName, data, null);
+        }
+
+        public void Exit()
+        {
+            if (exited != true)
+            {
+                connector.ExitRoom(roomName);
+
+                foreach (string fullMethodName in methodHandlerMap.Keys)
+                {
+                    List<object> methodHandlers = methodHandlerMap[fullMethodName];
+
+                    foreach (object methodHandler in methodHandlers)
+                    {
+                        connector.Off(fullMethodName, methodHandler);
+                    }
+
+                    methodHandlerMap.Remove(fullMethodName);
+                }
+
+                // free method handler map.
+                methodHandlerMap = null;
+
+                exited = true;
+            }
+        }
     }
 }
